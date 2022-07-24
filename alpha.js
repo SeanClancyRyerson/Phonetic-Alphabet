d58c2e801696a654da5b31d7ac2e4704075a9961
Sean Ryerson <seanclancyryerson@gmail.com>
2022-07-23 21:18:51 -0400

    Ad functions to read letters and say words

--- alpha.js
+++ alpha.js
@@ -1,6 +1,15 @@
-function sayWord() {}
+function sayWord() {
+  let word = document.getElementById("inputText").value;
+  word = word.toLowerCase();
+  for (let i = 0; i < word.length; i++) {
+    let letter = word[i];
+    if (letter.match(/[a-z]/i)) {
+      sayLetter(letter);
+    }
+  }
+}
 
-function sayLetter() {
-  var audio = new Audio("./sounds/at.mp3");
+function sayLetter(letter) {
+  var audio = new Audio(`./sounds/${letter}.mp3`);
   audio.play();
 }
