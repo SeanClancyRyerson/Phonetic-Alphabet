482d26383b7ff8343af27523d682ba3ea2db583c
Sean Ryerson <seanclancyryerson@gmail.com>
2022-07-23 21:23:58 -0400

    Add delay function between letters

--- alpha.js
+++ alpha.js
@@ -1,10 +1,11 @@
-function sayWord() {
+async function sayWord() {
   let word = document.getElementById("inputText").value;
   word = word.toLowerCase();
   for (let i = 0; i < word.length; i++) {
     let letter = word[i];
     if (letter.match(/[a-z]/i)) {
       sayLetter(letter);
+      await delay(800);
     }
   }
 }
@@ -12,4 +13,10 @@ function sayWord() {
 function sayLetter(letter) {
   var audio = new Audio(`./sounds/${letter}.mp3`);
   audio.play();
+}
+
+function delay(milliseconds) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
 }
